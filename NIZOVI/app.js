79b1bf4396180841ec31169c4b3fef3778846643
Amela <zupicamela@gmail.com>
2023-11-12 18:01:00 +0100

    first commit

---
+++ NIZOVI/app.js
@@ -0,0 +1,31 @@
+// niz = [1,2,3,4,5,6,7,8,9]
+// let niz2 = [1,2,3,4,5]
+// console.log(niz.concat(niz2));
+
+// console.log(niz.slice(0, 4));
+// console.log(niz.join(""));
+// console.log(niz.revese());
+
+// let nesto = niz.push(-1,-2,-3)
+// console.log(niz);
+// console.log(nesto);
+
+// let izbrisani = niz.pop()
+// console.log(niz)
+// console.log(izbrisani)
+
+// let shiftovani = niz.shift();
+// console.log(niz);
+// console.log(shiftovani);
+
+// niz.unshift(0)
+// console.log(niz)
+
+// delete niz[3];
+
+// let niz = [[1,2,3], [4,5,6], [7,8,9]]
+// console.log(niz.flat())
+
+niz = [1,2,3,4,5,6,7,8,9]
+
+console.log(niz.splice(3, 2, -10))
